cb9a686b673463e0c7be16d37c4a26456c9cda9e
Mark Mulder <markmulder@gmail.com>
2016-04-29 14:36:16 +0200

    Add example code

---
+++ decode.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Payload struct {
+	Icon  string `json:"icon.png"`
+	Pass  string `json:"pass.json"`
+	Strip string `json:"strip.png"`
+}
+
+func handleReceive(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal("ReadAll", err)
+	}
+
+	p := Payload{}
+	err = json.Unmarshal(body, &p)
+	fmt.Printf("%#v\n", p)
+	fmt.Println("Icon:", p.Icon)
+
+	// Build response object
+	resp := map[string]string{
+		"status": "ok",
+	}
+
+	output, err := json.MarshalIndent(resp, "", "  ")
+	if err != nil {
+		log.Fatal("MarshalIndent", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Write(output)
+}
+
+func main() {
+	http.HandleFunc("/receive", handleReceive)
+
+	fmt.Println("Listening on port 8000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe", err)
+	}
+}
---
+++ post.sh
@@ -0,0 +1,1 @@
+curl -H "Content-Type: application/json" -X POST -d @payload.json http://localhost:8000/receive
